7667f227257cda75d7f8743020f45b0dbaabdcb8
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-06 04:22:44 +0300

    Update test_yemen.py

--- tests/event_lists/test_yemen.py
+++ tests/event_lists/test_yemen.py
@@ -5,6 +5,13 @@ from load_one_data import ye_test_one_dataset
 data = {
     "Category:Yemen national football team managers": "تصنيف:مدربو منتخب اليمن لكرة القدم",
     "Category:Yemeni national football team managers": "تصنيف:مدربو منتخب اليمن لكرة القدم",
+
+    "Category:Yemen national softball team managers": "تصنيف:مدربو منتخب اليمن للكرة اللينة",
+    "Category:Yemeni national softball team managers": "تصنيف:مدربو منتخب اليمن للكرة اللينة",
+
+    "Category:United States national softball team managers": "تصنيف:مدربو منتخب الولايات المتحدة للكرة اللينة",
+    "Category:American national softball team managers": "تصنيف:مدربو منتخب الولايات المتحدة للكرة اللينة",
+
 }
 
 
