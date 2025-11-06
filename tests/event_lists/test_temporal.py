c91ee2e78a84d98cebd3fab4876035bea194f825
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-06 05:26:08 +0300

    1

--- tests/event_lists/test_temporal.py
+++ tests/event_lists/test_temporal.py
@@ -2,19 +2,16 @@
 from src import new_func_lab_final_label
 from load_one_data import ye_test_one_dataset, dump_diff
 
-data ={
+data = {
     "Category:00s establishments in the Roman Empire": "تصنيف:تأسيسات عقد 00 في الإمبراطورية الرومانية",
     "Category:1000 disestablishments by country": "تصنيف:انحلالات سنة 1000 حسب البلد",
     "Category:1000 disestablishments in Europe": "تصنيف:انحلالات سنة 1000 في أوروبا",
     "Category:1000s disestablishments in Asia": "تصنيف:انحلالات عقد 1000 في آسيا",
-    "Category:10th millennium in fiction": "تصنيف:الألفية 10 في الخيال",
     "Category:10th-century BC architecture": "تصنيف:عمارة القرن 10 ق م",
-    "Category:1270s in the Holy Roman Empire": "تصنيف:عقد 1270 في الإمبراطورية الرومانية المقدسة",
     "Category:1370s conflicts": "تصنيف:نزاعات عقد 1370",
     "Category:13th century establishments in the Roman Empire": "تصنيف:تأسيسات القرن 13 في الإمبراطورية الرومانية",
     "Category:14th-century establishments in India": "تصنيف:تأسيسات القرن 14 في الهند",
     "Category:18th-century Dutch explorers": "تصنيف:مستكشفون هولنديون في القرن 18",
-    "Category:18th-century people of the Dutch Empire": "تصنيف:أشخاص من الإمبراطورية الهولندية في القرن 18",
     "Category:1902 films": "تصنيف:أفلام إنتاج 1902",
     "Category:1950s criminal comedy films": "تصنيف:أفلام كوميديا الجريمة عقد 1950",
     "Category:1960s black comedy films": "تصنيف:أفلام كوميدية سوداء عقد 1960",
@@ -27,9 +24,7 @@ data ={
     "Category:1990s BC disestablishments in Asia": "تصنيف:انحلالات عقد 1990 ق م في آسيا",
     "Category:1990s disestablishments in Europe": "تصنيف:انحلالات عقد 1990 في أوروبا",
     "Category:1994–95 in European rugby union by country": "تصنيف:اتحاد الرجبي الأوروبي في 1994–95 حسب البلد",
-    "Category:19th-century actors by religion": "تصنيف:ممثلون حسب الدين في القرن 19",
     "Category:19th-century actors": "تصنيف:ممثلون في القرن 19",
-    "Category:19th-century people by religion": "تصنيف:أشخاص حسب الدين في القرن 19",
     "Category:19th-century publications": "تصنيف:منشورات القرن 19",
     "Category:1st century BC": "تصنيف:القرن 1 ق م",
     "Category:1st-century architecture": "تصنيف:عمارة القرن 1",
@@ -39,12 +34,10 @@ data ={
     "Category:2000s films": "تصنيف:أفلام إنتاج عقد 2000",
     "Category:2000s in American cinema": "تصنيف:السينما الأمريكية في عقد 2000",
     "Category:2000s in film": "تصنيف:عقد 2000 في الأفلام",
-    "Category:2000s in the United States by state": "تصنيف:عقد 2000 في الولايات المتحدة حسب الولاية",
     "Category:2006 Winter Paralympics events": "تصنيف:أحداث الألعاب البارالمبية الشتوية 2006",
     "Category:2006 establishments by country": "تصنيف:تأسيسات سنة 2006 حسب البلد",
     "Category:2006 in Northern Ireland sport": "تصنيف:رياضة أيرلندية شمالية في 2006",
     "Category:2006 in north korean sport": "تصنيف:رياضة كورية شمالية في 2006",
-    "Category:2015 American television": "تصنيف:2015 التلفزة الأمريكية",
     "Category:2017 American television episodes": "تصنيف:حلقات تلفزيونية أمريكية 2017",
     "Category:2017 American television seasons": "تصنيف:مواسم تلفزيونية أمريكية 2017",
     "Category:2017 American television series debuts": "تصنيف:مسلسلات تلفزيونية أمريكية بدأ عرضها في 2017",
@@ -56,13 +49,9 @@ data ={
     "Category:2017 sports events": "تصنيف:أحداث رياضية 2017",
     "Category:2017–18 in Emirati football": "تصنيف:كرة القدم الإماراتية في 2017–18",
     "Category:2018 Summer Youth Olympics events": "تصنيف:أحداث الألعاب الأولمبية الشبابية الصيفية 2018",
-    "Category:20th-century Albanian sports coaches": "تصنيف:مدربون رياضيون ألبان في القرن 20",
     "Category:20th-century disestablishments in India": "تصنيف:انحلالات القرن 20 في الهند",
-    "Category:20th-century railway accidents": "تصنيف:حوادث سكك حديدية القرن 20",
     "Category:21st century in film": "تصنيف:القرن 21 في الأفلام",
-    "Category:21st century in the Czech Republic": "تصنيف:القرن 21 في التشيك",
     "Category:21st-century films": "تصنيف:أفلام القرن 21",
-    "Category:21st-century in Qatar": "تصنيف:القرن 21 في قطر",
     "Category:440s bc": "تصنيف:عقد 440 ق م",
     "Category:440s": "تصنيف:عقد 440",
     "Category:977 by country": "تصنيف:977 حسب البلد",
@@ -70,7 +59,6 @@ data ={
     "Category:American cinema by decade": "تصنيف:السينما الأمريكية حسب العقد",
     "Category:Animals by year of formal description": "تصنيف:حيوانات حسب سنة الوصف",
     "Category:April 1983 events in Europe": "تصنيف:أحداث أبريل 1983 في أوروبا",
-    "Category:April 1983 sports events": "تصنيف:أحداث رياضية أبريل 1983",
     "Category:Comics set in the 1st century BC": "تصنيف:قصص مصورة تقع أحداثها في القرن 1 ق م",
     "Category:Decades by country": "تصنيف:عقود حسب البلد",
     "Category:Decades in Oklahoma": "تصنيف:عقود في أوكلاهوما",
@@ -79,15 +67,12 @@ data ={
     "Category:Historical webcomics": "تصنيف:ويب كومكس تاريخية",
     "Category:July 2018 events by continent": "تصنيف:أحداث يوليو 2018 حسب القارة",
     "Category:Mammals by century of formal description": "تصنيف:ثدييات حسب قرن الوصف",
-    "Category:Manufacturing companies established in the 2nd millennium": "تصنيف:شركات تصنيع أنشئت في الألفية 2",
     "Category:Multi-sport events in the Soviet Union": "تصنيف:أحداث رياضية متعددة في الاتحاد السوفيتي",
     "Category:November 2006 in Yemen": "تصنيف:نوفمبر 2006 في اليمن",
     "Category:Olympic figure skaters by year": "تصنيف:متزلجون فنيون أولمبيون حسب السنة",
     "Category:Publications by year of disestablishment": "تصنيف:منشورات حسب سنة الانحلال",
     "Category:Publications by year of establishment": "تصنيف:منشورات حسب سنة التأسيس",
-    "Category:Soviet Union at multi-sport events": "تصنيف:الاتحاد السوفيتي في الأحداث الرياضية المتعددة",
     "Category:Sports organisations by decade of establishment": "تصنيف:منظمات رياضية حسب عقد التأسيس",
-    "Category:Swaziland at multi-sport events": "تصنيف:سوازيلاند في الأحداث الرياضية المتعددة",
     "Category:Television series endings by year": "تصنيف:مسلسلات تلفزيونية حسب سنة انتهاء العرض",
     "Category:Tetrapods by century of formal description": "تصنيف:رباعيات الأطراف حسب قرن الوصف",
     "Category:Vertebrates described in the 20th century": "تصنيف:فقاريات وصفت في القرن 20",
@@ -102,4 +87,31 @@ def test_temporal():
     org, diff = ye_test_one_dataset(data, new_func_lab_final_label)
 
     dump_diff(diff, "test_temporal")
+    assert diff == org, f"Differences found: {len(diff)}"
+
+
+def test_temporal_2():
+
+    data = {
+        "Category:10th millennium in fiction": "تصنيف:الخيال في الألفية 10",
+        "Category:1270s in the Holy Roman Empire": "تصنيف:الإمبراطورية الرومانية المقدسة في عقد 1270",
+        "Category:18th-century people of the Dutch Empire": "تصنيف:أشخاص من الإمبراطورية الهولندية القرن 18",
+        "Category:19th-century actors by religion": "تصنيف:ممثلون في القرن 19 حسب الدين",
+        "Category:19th-century people by religion": "تصنيف:أشخاص في القرن 19 حسب الدين",
+        "Category:2000s in the United States by state": "تصنيف:الولايات المتحدة في عقد 2000 حسب الولاية",
+        "Category:2015 American television": "تصنيف:التلفزة الأمريكية 2015",
+        "Category:20th-century Albanian sports coaches": "تصنيف:مدربو رياضة ألبان في القرن 20",
+        "Category:20th-century railway accidents": "تصنيف:حوادث سكك حديد في القرن 20",
+        "Category:21st century in the Czech Republic": "تصنيف:التشيك في القرن 21",
+        "Category:21st-century in Qatar": "تصنيف:قطر في القرن 21",
+        "Category:April 1983 sports events": "تصنيف:أحداث أبريل 1983 الرياضية",
+        "Category:Manufacturing companies established in the 2nd millennium": "تصنيف:شركات تصنيع أسست في الألفية الثانية",
+        "Category:Soviet Union at multi-sport events": "تصنيف:أحداث الاتحاد السوفيتي في رياضية متعددة",
+        "Category:Swaziland at multi-sport events": "تصنيف:أحداث سوازيلاند في رياضية متعددة",
+    }
+
+    print(f"len of data: {len(data)}")
+    org, diff = ye_test_one_dataset(data, new_func_lab_final_label)
+
+    dump_diff(diff, "test_temporal_2")
     assert diff == org, f"Differences found: {len(diff)}"
