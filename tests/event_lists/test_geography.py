ae7e9ef91a66e941537d0d970d9591c8770a4bb8
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-06 05:07:21 +0300

    1

--- tests/event_lists/test_geography.py
+++ tests/event_lists/test_geography.py
@@ -3,6 +3,7 @@ from src import new_func_lab_final_label
 from load_one_data import ye_test_one_dataset, dump_diff
 
 data = {
+
     "Category:African-American history by state": "تصنيف:تاريخ أمريكي إفريقي حسب الولاية",
     "Category:Airlines by dependent territory": "تصنيف:شركات طيران حسب الأقاليم التابعة",
     "Category:Ambassadors by country of origin": "تصنيف:سفراء حسب البلد الأصل",
@@ -60,8 +61,8 @@ data = {
     "Category:Television series by country of shooting location": "تصنيف:مسلسلات تلفزيونية حسب بلد التصوير",
     "Category:Television shows by city of setting": "تصنيف:عروض تلفزيونية حسب مدينة الأحداث",
     "Category:Television stations by country": "تصنيف:محطات تلفزيونية حسب البلد",
-    "Category:films by country": "تصنيف:أفلام حسب البلد",
     "Category:books about politics by country": "تصنيف:كتب عن سياسة حسب البلد",
+    "Category:films by country": "تصنيف:أفلام حسب البلد",
 }
 
 
@@ -130,5 +131,66 @@ def test_geography_us():
     print(f"len of data: {len(data)}")
     org, diff = ye_test_one_dataset(data, new_func_lab_final_label)
 
-    dump_diff(diff, "test_geography")
+    dump_diff(diff, "test_geography_us")
+    assert diff == org, f"Differences found: {len(diff)}"
+
+
+def test_geography_in():
+    data = {
+
+        "Category:Buildings and structures in the United States by state": "تصنيف:مبان ومنشآت في الولايات المتحدة حسب الولاية",
+        "Category:Buildings and structures in Westchester County, New York": "تصنيف:مبان ومنشآت في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Cemeteries in Westchester County, New York": "تصنيف:مقابر في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Centuries in the United States by state": "تصنيف:قرون في الولايات المتحدة حسب الولاية",
+        "Category:Christianity in Westchester County, New York": "تصنيف:المسيحية في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Churches in Westchester County, New York": "تصنيف:كنائس في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Communications in the United States by state": "تصنيف:الاتصالات في الولايات المتحدة حسب الولاية",
+        "Category:Companies based in Westchester County, New York": "تصنيف:شركات مقرها في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Crime in Pennsylvania": "تصنيف:جريمة في بنسلفانيا",
+        "Category:Crimes in Pennsylvania": "تصنيف:جرائم في بنسلفانيا",
+        "Category:Crimes in the United States by state": "تصنيف:جرائم في الولايات المتحدة حسب الولاية",
+        "Category:Disasters in the United States by state": "تصنيف:كوارث في الولايات المتحدة حسب الولاية",
+        "Category:Education in the United States by state": "تصنيف:التعليم في الولايات المتحدة حسب الولاية",
+        "Category:Education in Westchester County, New York": "تصنيف:التعليم في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Films set in China by city": "تصنيف:أفلام تقع أحداثها في الصين حسب المدينة",
+        "Category:Films set in Westchester County, New York": "تصنيف:أفلام تقع أحداثها في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Films shot in China by city": "تصنيف:أفلام مصورة في الصين حسب المدينة",
+        "Category:Forts in the United States by state": "تصنيف:حصون في الولايات المتحدة حسب الولاية",
+        "Category:Health in North Dakota": "تصنيف:الصحة في داكوتا الشمالية",
+        "Category:Health in the United States by state": "تصنيف:الصحة في الولايات المتحدة حسب الولاية",
+        "Category:Historic districts in Westchester County, New York": "تصنيف:المناطق التاريخية في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Historic sites in the United States by state": "تصنيف:مواقع تاريخية في الولايات المتحدة حسب الولاية",
+        "Category:Historic trails and roads in the United States by state": "تصنيف:طرق وممرات تاريخية في الولايات المتحدة حسب الولاية",
+        "Category:Hospitals in Westchester County, New York": "تصنيف:مستشفيات في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Houses in Westchester County, New York": "تصنيف:منازل في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Landmarks in the United States by state": "تصنيف:معالم في الولايات المتحدة حسب الولاية",
+        "Category:Manufacturing in the United States by state": "تصنيف:تصنيع في الولايات المتحدة حسب الولاية",
+        "Category:Museums in Westchester County, New York": "تصنيف:متاحف في مقاطعة ويستتشستر (نيويورك)",
+        "Category:National Register of Historic Places in Westchester County, New York": "تصنيف:السجل الوطني للأماكن التاريخية في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Nature reserves in the United States by state": "تصنيف:محميات طبيعية في الولايات المتحدة حسب الولاية",
+        "Category:Olympic gold medalists for the United States in alpine skiing": "تصنيف:فائزون بميداليات ذهبية أولمبية من الولايات المتحدة في التزلج على المنحدرات الثلجية",
+        "Category:Parks in Westchester County, New York": "تصنيف:متنزهات في مقاطعة ويستتشستر (نيويورك)",
+        "Category:People by state in the United States": "تصنيف:أشخاص حسب الولاية في الولايات المتحدة",
+        "Category:Populated places in Westchester County, New York": "تصنيف:أماكن مأهولة في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Rail transport in Sri Lanka by province": "تصنيف:السكك الحديدية في سريلانكا حسب المقاطعة",
+        "Category:Riots and civil disorder in the United States by state": "تصنيف:شغب وعصيان مدني في الولايات المتحدة حسب الولاية",
+        "Category:Schools in Westchester County, New York": "تصنيف:مدارس في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Science and technology in the United States by state": "تصنيف:العلوم والتكنولوجيا في الولايات المتحدة حسب الولاية",
+        "Category:Slavery in the United States by state": "تصنيف:العبودية في الولايات المتحدة حسب الولاية",
+        "Category:Sports in the United States by state": "تصنيف:الرياضة في الولايات المتحدة حسب الولاية",
+        "Category:Sports in Westchester County, New York": "تصنيف:ألعاب رياضية في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Sports venues in Westchester County, New York": "تصنيف:ملاعب رياضية في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Television shows set in Australia by city": "تصنيف:عروض تلفزيونية تقع أحداثها في أستراليا حسب المدينة",
+        "Category:Tourist attractions in the United States by state": "تصنيف:مواقع جذب سياحي في الولايات المتحدة حسب الولاية",
+        "Category:Tourist attractions in Westchester County, New York": "تصنيف:مواقع جذب سياحي في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Transportation buildings and structures in Westchester County, New York": "تصنيف:مبان ومنشآت نقل في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Transportation in the United States by state": "تصنيف:النقل في الولايات المتحدة حسب الولاية",
+        "Category:Transportation in Westchester County, New York": "تصنيف:النقل في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Universities and colleges in Westchester County, New York": "تصنيف:جامعات وكليات في مقاطعة ويستتشستر (نيويورك)",
+        "Category:Women's sports organizations in the United States": "تصنيف:منظمات الرياضات النسوية في الولايات المتحدة",
+    }
+    print(f"len of data: {len(data)}")
+    org, diff = ye_test_one_dataset(data, new_func_lab_final_label)
+
+    dump_diff(diff, "test_geography_in")
     assert diff == org, f"Differences found: {len(diff)}"
