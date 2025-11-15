02c51355b183f590c6682d5fe7b6f5455ff5806c
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-16 01:51:16 +0300

    Update reg_result.py

--- src/make2_bots/ma_bots/dodo_bots/reg_result.py
+++ src/make2_bots/ma_bots/dodo_bots/reg_result.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 from ...format_bots import Tit_ose_Nmaes
 from ....ma_lists.type_tables import basedtypeTable
 from ...reg_lines import tita, tita_year, ddd, tita_year_no_month
-from ....new.time_to_arabic import convert_time_to_arabic, match_time_ar, match_time_en
 
 safo = "|".join(list(basedtypeTable))
 
@@ -24,47 +23,41 @@ class Typies:
     cat_test: str
 
 
-def get_reg_result(category: str, _category_: str, category3: str, cat_test: str = "") -> Typies:
-    # ---
-    cat_test = category3
-    # ---
-    category3 = re.sub("Category:", "", category3, re.I)
-    _category_ = re.sub("Category:", "", _category_, re.I)
-    category = re.sub("Category:", "", category, re.I)
+def get_reg_result(category: str, cate_gory: str, cate3: str, cat_test: str) -> Typies:
     # ---
     Tita_year = tita_year
     # ---
-    test_month = re.sub(ddd, "", category, re.I)
+    test_month = re.sub(ddd, "", category.lower(), re.I)
     # ---
     if test_month == category:
         Tita_year = tita_year_no_month
     # ---
     reg_line_1 = tita + tita_other
-    reg_line_1 = reg_line_1
+    reg_line_1 = reg_line_1.lower()
     # ---
-    year = re.sub(Tita_year, r"\g<1>\g<2>", _category_, re.I)
-    # year = match_time_en(_category_)
+    year = re.sub(Tita_year, r"\g<1>\g<2>", cate_gory, re.I)
+    # year = match_time_en(cate_gory)
     # ---
-    typeo = re.sub(reg_line_1, r"\g<3>", _category_, re.I)
+    typeo = re.sub(reg_line_1, r"\g<3>", cate_gory, re.I)
     # ---
-    if year == _category_ or year == category3:
+    if year == cate_gory or year == cate3:
         year = ""
-    elif year and _category_.startswith("category:" + year):
-        cat_test = cat_test.replace(year, "")
+    elif year and cate_gory.startswith("category:" + year):
+        cat_test = cat_test.replace(year.lower(), "")
         tita_n = "category:" + year + tita_other
-        typeo = re.sub(tita_n, r"\g<1>", _category_, re.I)
+        typeo = re.sub(tita_n, r"\g<1>", cate_gory, re.I)
 
-    if typeo == _category_ or typeo == category3:
+    if typeo == cate_gory or typeo == cate3:
         typeo = ""
     # ---
-    In = re.sub(reg_line_1, r"\g<4>", _category_, re.I)
+    In = re.sub(reg_line_1, r"\g<4>", cate_gory, re.I)
     # ---
-    if In == _category_ or In == category3:
+    if In == cate_gory or In == cate3:
         In = ""
     # ---
-    country = re.sub(reg_line_1, r"\g<5>", _category_, re.I)
+    country = re.sub(reg_line_1, r"\g<5>", cate_gory, re.I)
     # ---
-    if country == _category_ or country == category3:
+    if country == cate_gory or country == cate3:
         country = ""
     # ---
     if In.strip() == "by":
