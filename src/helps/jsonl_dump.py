ab1f99cc0c523b58455430fe80ed2d21ac87dfdd
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-17 05:24:04 +0300

    1

---
+++ src/helps/jsonl_dump.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+!
+"""
+import jsonlines
+from pathlib import Path
+
+
+def save(path, data) -> str:
+    path = Path(path)
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+        with jsonlines.open(path, mode='w') as writer:
+            writer.write_all({})
+    # ---
+    with jsonlines.open(path, mode='a') as writer:
+        writer.write_all(data)
