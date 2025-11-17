67057627b3efbfaaf2833cf06973c9a738475702
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-17 03:54:38 +0300

    Update fax2.py

--- src/make2_bots/ma_bots/end_start_bots/fax2.py
+++ src/make2_bots/ma_bots/end_start_bots/fax2.py
@@ -29,7 +29,7 @@ def get_from_starts_dict(category3: str) -> Tuple[str, str, bool]:
     return category3, list_of_cat, Find_wd
 
 
-def get_from_endswith_dict(category3: str, data: Dict[str, str] = {}) -> Tuple[str, str, bool]:
+def get_from_endswith_dict(category3: str, data: Dict[str, Dict[str, Any]] = {}) -> Tuple[str, str, bool, bool]:
     list_of_cat = ""
     Find_wd = False
     Find_ko = False
@@ -37,12 +37,14 @@ def get_from_endswith_dict(category3: str, data: Dict[str, str] = {}) -> Tuple[s
     if not data:
         data = to_get_endswith
 
+    category3_original = category3
     for key, tab in data.items():
-        lab = tab["lab"]
-
         if category3.endswith(key):
-            list_of_cat = lab
-            category3 = category3.replace(key, "", 1)
+            list_of_cat = tab["lab"]
+
+            # precise removal
+            remove_key = tab.get("remove", key)
+            category3 = category3_original.replace(remove_key, "", 1)
             Find_wd = tab.get("Find_wd") is True
             Find_ko = tab.get("Find_ko") is True
             break
