b4a1182f5f50d04888519e01d98d619cbc83e5fe
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-18 01:18:27 +0300

    1

--- src/new/end_start_bots/utils.py
+++ src/new/end_start_bots/utils.py
@@ -7,7 +7,6 @@ from typing import Dict, Tuple, Any
 
 def get_from_starts_dict(category3: str, data: Dict[str, Dict[str, Any]]) -> Tuple[str, str, bool]:
     list_of_cat = ""
-    Find_wd = False
 
     category3_original = category3
 
@@ -25,16 +24,13 @@ def get_from_starts_dict(category3: str, data: Dict[str, Dict[str, Any]]) -> Tup
 
             category3 = category3_original[len(remove_key):]  # .lstrip()
 
-            Find_wd = tab.get("Find_wd") is True
             break
 
-    return category3, list_of_cat, Find_wd
+    return category3, list_of_cat
 
 
 def get_from_endswith_dict(category3: str, data: Dict[str, Dict[str, Any]]) -> Tuple[str, str, bool, bool]:
     list_of_cat = ""
-    Find_wd = False
-    Find_ko = False
 
     category3_original = category3
 
@@ -51,11 +47,8 @@ def get_from_endswith_dict(category3: str, data: Dict[str, Dict[str, Any]]) -> T
             # precise removal
             remove_key = tab.get("remove", key)
 
-            category3 = category3_original[: -len(remove_key)]#.strip()
-
-            Find_wd = tab.get("Find_wd") is True
-            Find_ko = tab.get("Find_ko") is True
+            category3 = category3_original[: -len(remove_key)]  # .strip()
 
             break
 
-    return category3, list_of_cat, Find_wd, Find_ko
+    return category3, list_of_cat
