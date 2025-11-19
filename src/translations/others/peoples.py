129513781019aabc2be794c6e0346232b744d542
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-19 05:50:52 +0300

    1

--- src/translations/others/peoples.py
+++ src/translations/others/peoples.py
@@ -2,29 +2,7 @@
 """
 from .peoples import People_key
 
-SELECT DISTINCT #?cat
-#?ar  ?humanLabel
-#?page_en ?page_ar
-#(concat('   "' , ?page_en , '":"' , ?ar  , '",')  as ?itemscds)
-(concat('   "' , ?page_en , '":"' , ?page_ar  , '",')  as ?itemscds)
-WHERE {
-    ?human wdt:P31 wd:Q5.#
-    #?human wdt:P31/wdt:P279* wd:Q515.#
-    #?human wdt:P31/wdt:P279* wd:Q486972.
-    ?human wdt:P910 ?cat .
-    #?cat wdt:P301 ?human.
-    {?cat rdfs:label ?page_ar .  FILTER((LANG(?page_ar)) = "ar") } UNION {
-    ?article2 schema:about ?cat ; schema:isPartOf <https://ar.wikipedia.org/> ; schema:name ?page_ar . }
-    #OPTIONAL { ?sitelink schema:about ?cat . ?sitelink schema:isPartOf <https://ar.wikipedia.org/> }
-    #OPTIONAL { ?sitelink schema:about ?cat . ?sitelink schema:inLanguage "ar" }
-    # but select items with no such article
-    #FILTER (!BOUND(?sitelink))
-    ?article schema:about ?cat ; schema:isPartOf <https://en.wikipedia.org/> ; schema:name ?page_en .
-    SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "ar,en" .
-    }
-    }
-LIMIT 10000
+Query at: [People_key.sql](queries/People_key.sql)
 """
 # ---
 from ..utils.json_dir import open_json
