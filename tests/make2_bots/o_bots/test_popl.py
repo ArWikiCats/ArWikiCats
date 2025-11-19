40c9b39497f2c2b139150a10fa88968dcec189b6
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-19 05:11:44 +0300

    1

--- tests/make2_bots/o_bots/test_popl.py
+++ tests/make2_bots/o_bots/test_popl.py
@@ -3,11 +3,24 @@ Tests
 """
 import pytest
 
-from src.make2_bots.o_bots.popl import work_peoples, make_people_lab
+from src.make2_bots.o_bots.popl import work_peoples, make_people_lab, work_peoples_old
 
 fast_data = {
     "christina aguilera video albums": "ألبومات فيديو كريستينا أغيليرا",
     "ed sheeran albums": "ألبومات إد شيران",
+    "a. r. rahman albums": "ألبومات أي.أر. رحمان",
+    "george h. w. bush administration cabinet members": "أعضاء مجلس وزراء إدارة جورج بوش الأب",
+    "george h. w. bush administration personnel": "موظفو إدارة جورج بوش الأب",
+    "george w. bush administration cabinet members": "أعضاء مجلس وزراء إدارة جورج دبليو بوش",
+    "george w. bush administration personnel": "موظفو إدارة جورج دبليو بوش",
+    "john quincy adams administration cabinet members": "أعضاء مجلس وزراء إدارة جون كوينسي آدامز",
+    "john quincy adams administration personnel": "موظفو إدارة جون كوينسي آدامز",
+    "lyndon b. johnson administration cabinet members": "أعضاء مجلس وزراء إدارة ليندون جونسون",
+    "lyndon b. johnson administration personnel": "موظفو إدارة ليندون جونسون",
+    "nusrat fateh ali khan albums": "ألبومات نصرت فتح علي خان",
+    "pope john paul ii albums": "ألبومات يوحنا بولس الثاني",
+    "william henry harrison administration cabinet members": "أعضاء مجلس وزراء إدارة ويليام هنري هاريسون",
+    "william henry harrison administration personnel": "موظفو إدارة ويليام هنري هاريسون",
 }
 
 
@@ -17,6 +30,9 @@ def test_fast_data(category, expected) -> None:
 
     label = work_peoples(category)
     assert label.strip() == expected
+
+    label2 = work_peoples_old(category)
+    assert label == label2
 
 
 def test_work_peoples():
