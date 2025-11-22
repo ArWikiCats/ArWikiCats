30e269616d5da8a1a7f656d1a2f52ea0694d30d0
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-23 02:57:22 +0300

    Update COUNTRY_YEAR.py

--- src/main_processers/categories_patterns/COUNTRY_YEAR.py
+++ src/main_processers/categories_patterns/COUNTRY_YEAR.py
@@ -14,8 +14,8 @@ COUNTRY_YEAR_DATA ={
     "Category:{YEAR1} disestablishments in {COUNTRY1}": "تصنيف:انحلالات سنة {YEAR1} في {COUNTRY1}",  # 4600
     "Category:{YEAR1} sports events in {COUNTRY1}": "تصنيف:أحداث {YEAR1} الرياضية في {COUNTRY1}",  # 6108
     "Category:{YEAR1} crimes in {COUNTRY1}": "تصنيف:جرائم {YEAR1} في {COUNTRY1}",  # 3966
-    "Category:{YEAR1} murders in {COUNTRY1}": "تصنيف:جرائم قتل في {COUNTRY1} {YEAR1}",  # 2076
-    "Category:{YEAR1} disasters in {COUNTRY1}": "تصنيف:كوارث في {COUNTRY1} {YEAR1}",  # 2140
+    "Category:{YEAR1} murders in {COUNTRY1}": "تصنيف:جرائم قتل في {COUNTRY1} في {YEAR1}",
+    "Category:{YEAR1} disasters in {COUNTRY1}": "تصنيف:كوارث في {COUNTRY1} في {YEAR1}",  # 2140
     "Category:{YEAR1} in {COUNTRY1} by month": "تصنيف:أحداث {YEAR1} في {COUNTRY1} حسب الشهر",  # 1808
     "Category:{YEAR1} elections in {COUNTRY1}": "تصنيف:انتخابات {COUNTRY1} في {YEAR1}",  # 1550
     "Category:{YEAR1} events in {COUNTRY1} by month": "تصنيف:أحداث {YEAR1} في {COUNTRY1} حسب الشهر",  # 1382
@@ -33,16 +33,16 @@ COUNTRY_YEAR_DATA ={
     "Category:{YEAR1} in New {COUNTRY1}": "تصنيف:{COUNTRY1} الجديدة في {YEAR1}",  # 253
     "Category:{YEAR1} in {COUNTRY1} by state": "تصنيف:{YEAR1} في {COUNTRY1} حسب الولاية",  # 280
     "Category:{YEAR1} in {COUNTRY1} by state or territory": "تصنيف:{COUNTRY1} في {YEAR1} حسب الولاية",  # 243
-    "Category:{YEAR1} mass shootings in {COUNTRY1}": "تصنيف:إطلاق نار عشوائي في {COUNTRY1} {YEAR1}",  # 215
+    "Category:{YEAR1} mass shootings in {COUNTRY1}": "تصنيف:إطلاق نار عشوائي في {COUNTRY1} في {YEAR1}",  # 215
     "Category:Attacks in {COUNTRY1} in {YEAR1}": "تصنيف:هجمات في {COUNTRY1} في {YEAR1}",  # 247
     "Category:{YEAR1} Roman Catholic bishops in {COUNTRY1}": "تصنيف:أساقفة كاثوليك رومان في {COUNTRY1} في {YEAR1}",  # 233
     "Category:{YEAR1} establishments in New {COUNTRY1}": "تصنيف:تأسيسات سنة {YEAR1} في {COUNTRY1} الجديدة",  # 154
     "Category:{YEAR1} in {COUNTRY1} City": "تصنيف:مدينة {COUNTRY1} في {YEAR1}",  # 150
-    "Category:{YEAR1} religious buildings and structures in {COUNTRY1}": "تصنيف:مبان ومنشآت دينية في {COUNTRY1} {YEAR1}",  # 165
-    "Category:{YEAR1} churches in {COUNTRY1}": "تصنيف:كنائس في {COUNTRY1} {YEAR1}",  # 172
+    "Category:{YEAR1} religious buildings and structures in {COUNTRY1}": "تصنيف:مبان ومنشآت دينية في {COUNTRY1} في {YEAR1}",  # 165
+    "Category:{YEAR1} churches in {COUNTRY1}": "تصنيف:كنائس في {COUNTRY1} في {YEAR1}",  # 172
     "Category:{YEAR1} in {COUNTRY1} (U.S. state)": "تصنيف:ولاية {COUNTRY1} في {YEAR1}",  # 155
     "Category:{COUNTRY1} at UEFA Euro {YEAR1}": "تصنيف:{COUNTRY1} في بطولة أمم أوروبا {YEAR1}",  # 183
-    "Category:{YEAR1} mosques in {COUNTRY1}": "تصنيف:مساجد في {COUNTRY1} {YEAR1}",  # 175
+    "Category:{YEAR1} mosques in {COUNTRY1}": "تصنيف:مساجد في {COUNTRY1} في {YEAR1}",  # 175
     "Category:{YEAR1} in sport in {COUNTRY1}": "تصنيف:أحداث {YEAR1} الرياضية في {COUNTRY1}",  # 143
     "Category:Terrorist incidents in {COUNTRY1} in the {YEAR1}": "تصنيف:حوادث إرهابية في {COUNTRY1} في {YEAR1}",  # 173
     "Category:{YEAR1} establishments in {COUNTRY1} (U.S. state)": "تصنيف:تأسيسات سنة {YEAR1} في ولاية {COUNTRY1}",  # 138
@@ -53,36 +53,36 @@ COUNTRY_YEAR_DATA ={
     "Category:{YEAR1} establishments in {COUNTRY1} City": "تصنيف:تأسيسات سنة {YEAR1} في مدينة {COUNTRY1}",  # 124
     "Category:{YEAR1} executions by {COUNTRY1}": "تصنيف:إعدامات في {COUNTRY1} في {YEAR1}",  # 96
     "Category:{YEAR1} people from {COUNTRY1}": "تصنيف:أشخاص من {COUNTRY1} في {YEAR1}",  # 115
-    "Category:{YEAR1} fires in {COUNTRY1}": "تصنيف:حرائق في {COUNTRY1} {YEAR1}",  # 120
+    "Category:{YEAR1} fires in {COUNTRY1}": "تصنيف:حرائق في {COUNTRY1} في {YEAR1}",  # 120
     "Category:{YEAR1} establishments in {COUNTRY1}, D.C.": "تصنيف:تأسيسات سنة {YEAR1} في {COUNTRY1} العاصمة",  # 112
     "Category:{YEAR1} {COUNTRY1} politicians": "تصنيف:سياسيو {COUNTRY1} في {YEAR1}",  # 88
     "Category:{YEAR1} in {COUNTRY1} by province or territory": "تصنيف:{COUNTRY1} في {YEAR1} حسب المقاطعة أو الإقليم",  # 137
     "Category:{YEAR1} mass murder in {COUNTRY1}": "تصنيف:قتل جماعي في {COUNTRY1} في {YEAR1}",  # 84
-    "Category:{YEAR1} Roman Catholic archbishops in {COUNTRY1}": "تصنيف:رؤساء أساقفة رومان كاثوليك في {COUNTRY1} {YEAR1}",  # 129
+    "Category:{YEAR1} Roman Catholic archbishops in {COUNTRY1}": "تصنيف:رؤساء أساقفة رومان كاثوليك في {COUNTRY1} في {YEAR1}",  # 129
     "Category:{YEAR1} in sports in {COUNTRY1} (state)": "تصنيف:الرياضة في ولاية {COUNTRY1} في {YEAR1}",  # 131
     "Category:{YEAR1} in sports in {COUNTRY1} City": "تصنيف:الرياضة في مدينة {COUNTRY1} في {YEAR1}",  # 126
-    "Category:{YEAR1} Tour de {COUNTRY1}": "تصنيف:سباق طواف {COUNTRY1} {YEAR1}",  # 110
+    "Category:{YEAR1} Tour de {COUNTRY1}": "تصنيف:سباق طواف {COUNTRY1} في {YEAR1}",  # 110
     "Category:{YEAR1} monarchs in {COUNTRY1}": "تصنيف:ملكيون في {COUNTRY1} في {YEAR1}",  # 82
     "Category:{YEAR1} in {COUNTRY1} (country)": "تصنيف:{COUNTRY1} في {YEAR1}",  # 99
     "Category:{YEAR1} disestablishments in {COUNTRY1} (state)": "تصنيف:انحلالات سنة {YEAR1} في ولاية {COUNTRY1}",  # 71
     "Category:{COUNTRY1} at the {YEAR1} FIFA Women's World Cup": "تصنيف:{COUNTRY1} في كأس العالم لكرة القدم للسيدات {YEAR1}",  # 97
-    "Category:{YEAR1} Roman Catholic church buildings in {COUNTRY1}": "تصنيف:مبان كنائس رومانية كاثوليكية في {COUNTRY1} {YEAR1}",  # 92
+    "Category:{YEAR1} Roman Catholic church buildings in {COUNTRY1}": "تصنيف:مبان كنائس رومانية كاثوليكية في {COUNTRY1} في {YEAR1}",  # 92
     "Category:{YEAR1} in Ottoman {COUNTRY1}": "تصنيف:{COUNTRY1} العثمانية في {YEAR1}",  # 65
-    "Category:{YEAR1} natural disasters in {COUNTRY1}": "تصنيف:كوارث طبيعية في {COUNTRY1} {YEAR1}",  # 84
-    "Category:{YEAR1} floods in {COUNTRY1}": "تصنيف:فيضانات في {COUNTRY1} {YEAR1}",  # 62
+    "Category:{YEAR1} natural disasters in {COUNTRY1}": "تصنيف:كوارث طبيعية في {COUNTRY1} في {YEAR1}",  # 84
+    "Category:{YEAR1} floods in {COUNTRY1}": "تصنيف:فيضانات في {COUNTRY1} في {YEAR1}",  # 62
     "Category:{YEAR1} awards in {COUNTRY1}": "تصنيف:جوائز {YEAR1} في {COUNTRY1}",  # 78
     "Category:Aviation accidents and incidents in {COUNTRY1} in {YEAR1}": "تصنيف:حوادث طيران في {COUNTRY1} في {YEAR1}",  # 83
     "Category:{YEAR1} establishments in {COUNTRY1} by state or union territory": "تصنيف:تأسيسات سنة {YEAR1} في {COUNTRY1} حسب الولاية أو الإقليم الاتحادي",  # 72
-    "Category:{YEAR1} {COUNTRY1} elections": "تصنيف:انتخابات {COUNTRY1} {YEAR1}",  # 79
-    "Category:Candidates in the {YEAR1} {COUNTRY1} elections": "تصنيف:مرشحون في انتخابات {COUNTRY1} {YEAR1}",  # 73
+    "Category:{YEAR1} {COUNTRY1} elections": "تصنيف:انتخابات {COUNTRY1} في {YEAR1}",  # 79
+    "Category:Candidates in the {YEAR1} {COUNTRY1} elections": "تصنيف:مرشحون في انتخابات {COUNTRY1} في {YEAR1}",  # 73
     "Category:{YEAR1} military history of {COUNTRY1}": "تصنيف:تاريخ {COUNTRY1} العسكري في {YEAR1}",  # 57
-    "Category:{YEAR1} controversies in {COUNTRY1}": "تصنيف:خلافات في {COUNTRY1} {YEAR1}",  # 53
+    "Category:{YEAR1} controversies in {COUNTRY1}": "تصنيف:خلافات في {COUNTRY1} في {YEAR1}",  # 53
     "Category:Attacks in {COUNTRY1} in the {YEAR1}": "تصنيف:هجمات في {COUNTRY1} في {YEAR1}",  # 64
     "Category:{YEAR1} members of {COUNTRY1} General Assembly": "تصنيف:أعضاء جمعية {COUNTRY1} العامة في {YEAR1}",  # 57
     "Category:{COUNTRY1} at the {YEAR1} Copa América": "تصنيف:{COUNTRY1} في كوبا أمريكا {YEAR1}",  # 57
     "Category:{YEAR1} in the Colony of {COUNTRY1}": "تصنيف:{COUNTRY1} في {YEAR1}",  # 54
     "Category:{YEAR1} festivals in {COUNTRY1}": "تصنيف:مهرجانات {YEAR1} في {COUNTRY1}",  # 55
     "Category:{YEAR1} members of {COUNTRY1} Legislature": "تصنيف:أعضاء هيئة {COUNTRY1} التشريعية في {YEAR1}",  # 54
-    "Category:{YEAR1} {COUNTRY1} state court judges": "تصنيف:قضاة محكمة ولاية {COUNTRY1} {YEAR1}",  # 54
+    "Category:{YEAR1} {COUNTRY1} state court judges": "تصنيف:قضاة محكمة ولاية {COUNTRY1} في {YEAR1}",  # 54
 }
 
