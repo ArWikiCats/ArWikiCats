47a26d54306636680b3f901007785189cdd6fd2e
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-25 05:54:35 +0300

    Update relegin_jobs.py

--- src/make2_bots/jobs_bots/te4_bots/relegin_jobs.py
+++ src/make2_bots/jobs_bots/te4_bots/relegin_jobs.py
@@ -35,17 +35,21 @@ def relegins_jobs(cate: str) -> str:
 def try_relegins_jobs_with_suffix(cate: str) -> str:
     """
     Try to generate religion job labels using nationality-style suffix logic.
-    TODO: use FormatData method"""
+    TODO: use FormatData method
+    """
 
     logger.debug(f"\t xx start: <<lightred>>try_relegins_jobs_with_suffix >> <<lightpurple>> cate:{cate}")
+
     category_suffix, country_prefix = get_con_3(cate, "religions")
+
     if not category_suffix:
         return ""
+
     Tab = RELIGIOUS_KEYS_PP.get(country_prefix, {})
+
     mens = Tab.get("mens")
     womens = Tab.get("womens")
     country_lab = jobs_with_nat_prefix(cate, country_prefix, category_suffix, mens=mens, womens=womens, find_nats=False)
+
     logger.debug(f"\t xx end: <<lightred>>try_relegins_jobs_with_suffix <<lightpurple>> cate:{cate}, country_lab:{country_lab} ")
-    # if country_lab:
-    # save(Path(__file__).parent / "relegin_jobs.jsonl", [{"cate": cate, "country_prefix": country_prefix, "category_suffix": category_suffix, "mens": mens, "womens": womens, "country_lab": country_lab}])
     return country_lab
