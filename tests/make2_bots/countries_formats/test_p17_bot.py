2574db81f61780ee88205c35d66704f5b438eaaa
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-25 05:54:40 +0300

    .

--- tests/make2_bots/countries_formats/test_p17_bot.py
+++ tests/make2_bots/countries_formats/test_p17_bot.py
@@ -4,7 +4,7 @@ Tests
 
 import pytest
 
-from src.make2_bots.p17_bots.p17_bot import Get_P17, Get_P17_2, add_definite_article
+from src.make2_bots.countries_formats.p17_bot import Get_P17, Get_P17_2, add_definite_article
 
 
 @pytest.mark.skip2
