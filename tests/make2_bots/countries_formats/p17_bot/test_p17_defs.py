989b40f23651a4b171d0d4ae652ac0c5af708066
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-26 04:36:17 +0300

    Update test_p17_defs.py

--- tests/make2_bots/countries_formats/p17_bot/test_p17_defs.py
+++ tests/make2_bots/countries_formats/p17_bot/test_p17_defs.py
@@ -92,7 +92,7 @@ TEMPORAL_CASES = [
 ]
 
 
-@pytest.mark.parametrize("name,data", TEMPORAL_CASES)
+@pytest.mark.parametrize("name,data, callback", TEMPORAL_CASES)
 def test_all_dump(name, data, callback):
     expected, diff_result = one_dump_test(data, callback)
 
