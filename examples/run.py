3c846c74f40ce5ff2e3af7b88011277abe8be995
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-29 08:59:48 +0300

    Update run.py

--- examples/run.py
+++ examples/run.py
@@ -1,3 +1,10 @@
+
+import sys
+from pathlib import Path
+
+if _Dir := Path(__file__).parent.parent:
+    sys.path.append(str(_Dir))
+
 from ArWikiCats import resolve_arabic_category_label as po
 
 print(po("Category:2015 American television"))
