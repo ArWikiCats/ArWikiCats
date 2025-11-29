c681a4ef4dfafe67aa9a0d19f194527515699c3a
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-30 02:18:23 +0300

    Update __init__.py

--- ArWikiCats/translations/sports/__init__.py
+++ ArWikiCats/translations/sports/__init__.py
@@ -4,7 +4,6 @@ from ..sports_formats_oioioi.data import (
     NAT_P17_OIOI,
 )
 from .games_labs import SUMMER_WINTER_GAMES
-from .nat_p17 import SPORT_FORMATS_FOR_P17
 from .olympics_data import olympics
 from .skeys import (
     SPORT_FORMTS_EN_AR_IS_P17,
@@ -21,7 +20,6 @@ __all__ = [
     "SPORT_FORMTS_NEW_KKK",
     "SPORT_FORMTS_MALE_NAT",
     "SPORT_FORMTS_FEMALE_NAT",
-    "SPORT_FORMATS_FOR_P17",
     "NAT_P17_OIOI",
     "SUMMER_WINTER_GAMES",
     "SPORTS_KEYS_FOR_TEAM",
