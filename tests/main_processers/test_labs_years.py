2a11e61a08c74790e39d136edfe89ac4ea517ff9
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-02 05:29:16 +0300

    Update test_labs_years.py

--- tests/main_processers/test_labs_years.py
+++ tests/main_processers/test_labs_years.py
@@ -45,6 +45,18 @@ def test_lab_from_year_year_detected_but_no_template() -> None:
     assert result == ("1999", "")
 
 
+def test_lab_from_year_add_creates() -> None:
+    """Should correctly create the template key/value with year replaced by {year1}."""
+    bot = LabsYears()
+
+    label = bot.lab_from_year("Category:1670-related list")
+
+    assert label == "تصنيف:قوائم متعلقة ب1670"
+
+    label2 = bot.lab_from_year("Category:1670s-related list")
+    assert label2 == "تصنيف:قوائم متعلقة بعقد 1670"
+
+
 def test_lab_from_year_add_creates_template() -> None:
     """Should correctly create the template key/value with year replaced by {year1}."""
     bot = LabsYears()
