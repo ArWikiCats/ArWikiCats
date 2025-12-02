aa5dbe0c192c2460bad798e406e7478a00e6e8f7
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-02 05:41:35 +0300

    1

--- ArWikiCats/main_processers/labs_years.py
+++ ArWikiCats/main_processers/labs_years.py
@@ -53,6 +53,7 @@ class MatchTimes:
         """Fix text."""
         text = re.sub(r"(انحلالات|تأسيسات)\s*سنة\s*(عقد|القرن|الألفية)", r"\g<1> \g<2>", text)
         text = text.replace("بعقد عقد", "بعقد")
+        text = text.replace("بعقد القرن", "بالقرن")
         return text
 
 
--- ArWikiCats/translations_formats/DataModel/model_data_time.py
+++ ArWikiCats/translations_formats/DataModel/model_data_time.py
@@ -57,6 +57,7 @@ class YearFormatData:
         """Fix text."""
         text = re.sub(r"(انحلالات|تأسيسات)\s*سنة\s*(عقد|القرن|الألفية)", r"\g<1> \g<2>", text)
         text = text.replace("بعقد عقد", "بعقد")
+        text = text.replace("بعقد القرن", "بالقرن")
         return text
 
     def replace_value_placeholder(self, label: str, value: str) -> str:
