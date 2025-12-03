1aa45c7993c2f1ffe322ba508266e7ac0d938c82
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-03 08:26:39 +0300

    1

--- ArWikiCats/main_processers/categories_patterns/NAT.py
+++ ArWikiCats/main_processers/categories_patterns/NAT.py
@@ -5,6 +5,14 @@ NAT_PARAMS =[
     "{nat_men1}"
 ]
 
+NAT_DATA_to_check = {
+    "{en_nat} singers": "مغنون ومغنيات {nat_men1}",  # 84
+    "{en_nat} actors": "ممثلون وممثلات {nat_men1}",  # 90
+    "{en_nat} boxers": "ملاكمون وملاكمات {nat_men1}",  # 136
+    "{en_nat} cyclists": "دراجون ودراجات {nat_men1}",  # 68
+    "{en_nat} actors by medium": "ممثلون وممثلات {nat_men1} حسب الوسط",  # 65
+
+}
 NAT_DATA = {
     "{en_nat} people": "{nat_men1}",  # 187
     "{en_nat} people by occupation": "{nat_men1} حسب المهنة",  # 182
@@ -32,8 +40,6 @@ NAT_DATA = {
     "{en_nat} expatriate men's footballers": "لاعبو كرة قدم {nat_men1} مغتربون",  # 163
     "{en_nat} people by century": "{nat_men1} حسب القرن",  # 121
     "{en_nat} people by political orientation": "{nat_men1} حسب التوجه السياسي",  # 156
-    "{en_nat} singers": "مغنون ومغنيات {nat_men1}",  # 84
-    "{en_nat} actors": "ممثلون وممثلات {nat_men1}",  # 90
     "{en_nat} christians": "مسيحيون {nat_men1}",  # 134
     "{en_nat} political people": "ساسة {nat_men1}",  # 160
     "{en_nat} military personnel": "عسكريون {nat_men1}",  # 106
@@ -41,30 +47,23 @@ NAT_DATA = {
     "{en_nat} lawyers": "محامون {nat_men1}",  # 158
     "{en_nat} athletes": "لاعبو قوى {nat_men1}",  # 79
     "{en_nat} football managers": "مدربو كرة قدم {nat_men1}",  # 154
-    "{en_nat} male martial artists": "ممارسو فنون قتالية {nat_men1}",  # 137
     "{en_nat} martial artists": "ممارسو وممارسات فنون قتالية {nat_men1}",  # 120
     "{en_nat} prisoners and detainees": "سجناء ومعتقلون {nat_men1}",  # 153
     "{en_nat} scientists": "علماء {nat_men1}",  # 151
     "{en_nat} artists": "فنانون {nat_men1}",  # 129
-    "{en_nat} boxers": "ملاكمون وملاكمات {nat_men1}",  # 136
     "{en_nat} businesspeople": "أصحاب أعمال {nat_men1}",  # 110
     "{en_nat} swimmers": "سباحون {nat_men1}",  # 112
     "{en_nat} journalists": "صحفيون {nat_men1}",  # 138
-    "{en_nat} male boxers": "ملاكمون {nat_men1}",  # 136
-    "{en_nat} male athletes": "لاعبو قوى {nat_men1}",  # 81
     "{en_nat} runners": "عداؤون {nat_men1}",  # 142
     "{en_nat} poets": "شعراء {nat_men1}",  # 86
     "{en_nat} people by occupation and century": "{nat_men1} حسب المهنة والقرن",  # 130
     "{en_nat} film directors": "مخرجو أفلام {nat_men1}",  # 134
     "{en_nat} educators": "معلمون {nat_men1}",  # 80
-    "{en_nat} male swimmers": "سباحون ذكور {nat_men1}",  # 101
     "{en_nat} competitors by sports event": "منافسون {nat_men1} حسب الحدث الرياضي",  # 136
     "{en_nat} academics": "أكاديميون {nat_men1}",  # 136
-    "{en_nat} male actors": "ممثلون {nat_men1}",  # 91
     "{en_nat} novelists": "روائيون {nat_men1}",  # 101
     "{en_nat} sprinters": "عداؤون سريعون {nat_men1}",  # 85
     "{en_nat} muslims": "مسلمون {nat_men1}",  # 132
-    "{en_nat} male singers": "مغنون {nat_men1}",  # 85
     "{en_nat} non-fiction writers": "كتاب غير روائيين {nat_men1}",  # 118
     "{en_nat} criminals": "مجرمون {nat_men1}",  # 124
     "{en_nat} murder victims": "ضحايا قتل {nat_men1}",  # 128
@@ -75,9 +74,7 @@ NAT_DATA = {
     "{en_nat} basketball players": "لاعبو ولاعبات كرة سلة {nat_men1}",  # 65
     "{en_nat} judges": "قضاة {nat_men1}",  # 125
     "{en_nat} victims of crime": "ضحايا جرائم {nat_men1}",  # 123
-    "{en_nat} male freestyle swimmers": "سباحو تزلج حر ذكور {nat_men1}",  # 121
     "{en_nat} economists": "اقتصاديون {nat_men1}",  # 124
-    "{en_nat} male sprinters": "عداؤون سريعون ذكور {nat_men1}",  # 71
     "{en_nat} mass media people": "إعلاميون {nat_men1}",  # 122
     "{en_nat} people by century and occupation": "{nat_men1} حسب القرن والمهنة",  # 105
     "{en_nat} writers by century": "كتاب {nat_men1} حسب القرن",  # 122
@@ -86,14 +83,11 @@ NAT_DATA = {
     "{en_nat} men's basketball players": "لاعبو كرة سلة {nat_men1}",  # 64
     "{en_nat} film actors": "ممثلو وممثلات أفلام {nat_men1}",  # 74
     "{en_nat} human rights activists": "{nat_men1} ناشطون في حقوق الإنسان",  # 108
-    "{en_nat} male writers": "كتاب {nat_men1}",  # 86
     "{en_nat} composers": "ملحنون {nat_men1}",  # 102
     "{en_nat} physicians": "أطباء {nat_men1}",  # 117
-    "{en_nat} actors by medium": "ممثلون وممثلات {nat_men1} حسب الوسط",  # 65
     "{en_nat} feminists": "نسويون {nat_men1}",  # 115
     "{en_nat} historians": "مؤرخون {nat_men1}",  # 114
     "{en_nat} communists": "شيوعيون {nat_men1}",  # 113
-    "{en_nat} male film actors": "ممثلو أفلام {nat_men1}",  # 80
     "{en_nat} tennis players": "لاعبو ولاعبات كرة مضرب {nat_men1}",  # 87
     "{en_nat} people of german descent": "{nat_men1} من أصل ألماني",  # 109
     "executed {en_nat} people": "{nat_men1} معدومون",  # 67
@@ -111,7 +105,6 @@ NAT_DATA = {
     "{en_nat} short story writers": "كتاب قصة قصيرة {nat_men1}",  # 71
     "{en_nat} actors by century": "ممثلون {nat_men1} حسب القرن",  # 69
     "{en_nat} murderers": "قتلة {nat_men1}",  # 103
-    "{en_nat} cyclists": "دراجون ودراجات {nat_men1}",  # 68
     "{en_nat} producers": "منتجون {nat_men1}",  # 103
     "{en_nat} musicians by instrument": "موسيقيون {nat_men1} حسب الآلة",  # 97
     "{en_nat} architects": "معماريون {nat_men1}",  # 102
@@ -121,4 +114,20 @@ NAT_DATA = {
     "{en_nat} middle-distance runners": "عداؤو مسافات متوسطة {nat_men1}",  # 65
     "{en_nat} civil servants": "موظفو خدمة مدنية {nat_men1}",  # 100
     "{en_nat} nationalists": "قوميون {nat_men1}",  # 99
+
+    # males with ذكور
+    "{en_nat} male swimmers": "سباحون ذكور {nat_men1}",  # 101
+    "{en_nat} male freestyle swimmers": "سباحو تزلج حر ذكور {nat_men1}",  # 121
+    "{en_nat} male sprinters": "عداؤون سريعون ذكور {nat_men1}",  # 71
+
+
+    # males without ذكور
+    "{en_nat} male martial artists": "ممارسو فنون قتالية ذكور {nat_men1}",  # 137
+    "{en_nat} male boxers": "ملاكمون ذكور {nat_men1}",  # 136
+    "{en_nat} male athletes": "لاعبو قوى ذكور {nat_men1}",  # 81
+    "{en_nat} male actors": "ممثلون ذكور {nat_men1}",  # 91
+    "{en_nat} male singers": "مغنون ذكور {nat_men1}",  # 85
+    "{en_nat} male writers": "كتاب ذكور {nat_men1}",  # 86
+    "{en_nat} male film actors": "ممثلو أفلام ذكور {nat_men1}",  # 80
+
 }
