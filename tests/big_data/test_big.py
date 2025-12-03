bbe1d27f9d023585ddc6bae5e3fd3c9704506858
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-03 07:43:26 +0300

    1

---
+++ tests/big_data/test_big.py
@@ -0,0 +1,27 @@
+"""
+pytest tests/big_data/test_big.py -m skip2
+"""
+
+import pytest
+import json
+from pathlib import Path
+from load_one_data import dump_diff, one_dump_test
+
+from ArWikiCats import resolve_arabic_category_label
+
+ENTERTAINMENT_CASES = []
+
+json_files = [x for x in (Path(__file__).parent).glob("*.json")]
+
+for file in json_files:
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    ENTERTAINMENT_CASES.append((f"test_big_{file.name}", data))
+
+
+@pytest.mark.parametrize("name,data", ENTERTAINMENT_CASES)
+@pytest.mark.skip2
+def test_entertainment(name: str, data: dict[str, str]) -> None:
+    expected, diff_result = one_dump_test(data, resolve_arabic_category_label)
+    dump_diff(diff_result, name)
+    assert diff_result == expected, f"Differences found: {len(diff_result)}"
