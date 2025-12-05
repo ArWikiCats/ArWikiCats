6739a5e72a718b143e94876a8215e5a55b2ab627
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-06 00:33:34 +0300

    Rename federation_bot to nats_sports_males
    Renamed the federation_bot module and its corresponding test to nats_sports_males for clarity and consistency. Updated imports and references in __init__.py to reflect the new module name.

--- ArWikiCats/translations_resolvers/__init__.py
+++ ArWikiCats/translations_resolvers/__init__.py
@@ -1,6 +1,6 @@
 
 from .nats_women import nats_women_label
-from . import countries_names, federation_bot
+from . import countries_names, nats_sports_males
 
 
 def resolved_sports_formats_labels(normalized_category) -> str:
@@ -8,7 +8,7 @@ def resolved_sports_formats_labels(normalized_category) -> str:
     resolved_label = countries_names.resolve_by_countries_names(normalized_category)
 
     if not resolved_label:
-        resolved_label = federation_bot.resolve_federation_label(normalized_category)
+        resolved_label = nats_sports_males.resolve_federation_label(normalized_category)
 
     if not resolved_label:
         resolved_label = nats_women_label(normalized_category)
