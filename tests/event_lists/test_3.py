c8fec6c30e3ab6fa163a65bb232040192b343eb3
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-07 01:15:57 +0300

    Update test_3.py

--- tests/event_lists/test_3.py
+++ tests/event_lists/test_3.py
@@ -11,6 +11,14 @@ data0 = {
 }
 
 data1 = {
+    "ivorian people": "تصنيف:أعلام إيفواريون",
+    "japanese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو يابانيون",
+    "chinese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو صينيون",
+    "arab lgbtq people": "تصنيف:أعلام إل جي بي تي كيو عرب",
+    "rwandan political people": "تصنيف:ساسة روانديون",
+    "maldivian people": "تصنيف:أعلام مالديفيون",
+    "malian people": "تصنيف:أعلام ماليون",
+    "maltese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو مالطيون",
     "namibia–russia relations": "تصنيف:العلاقات الروسية الناميبية",
     "morocco–netherlands relations": "تصنيف:العلاقات المغربية الهولندية",
     "morocco–pakistan relations": "تصنيف:العلاقات الباكستانية المغربية",
@@ -151,14 +159,6 @@ data1 = {
 }
 
 data_2 = {
-    "ivorian people": "تصنيف:أعلام إيفواريون",
-    "japanese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو يابانيون",
-    "chinese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو صينيون",
-    "arab lgbtq people": "تصنيف:أعلام إل جي بي تي كيو عرب",
-    "rwandan political people": "تصنيف:ساسة روانديون",
-    "maldivian people": "تصنيف:أعلام مالديفيون",
-    "malian people": "تصنيف:أعلام ماليون",
-    "maltese lgbtq people": "تصنيف:أعلام إل جي بي تي كيو مالطيون"
 
 }
 
