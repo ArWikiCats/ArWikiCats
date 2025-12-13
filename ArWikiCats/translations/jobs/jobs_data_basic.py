fa9fbba680720dc72741c945edf8f1bcdf64f931
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-13 03:28:34 +0300

    Add 'baháís' key and expand religion job tests
    Added the 'baháís' key to RELIGIOUS_KEYS_PP for alternate spelling support. Updated tests to include 'baháís' cases and refactored test_relegin_jobs_with_suffix.py to test both old and new suffix functions, add @pytest.mark.fast, and reorganize expatriates tests with @pytest.mark.skip2.

--- ArWikiCats/translations/jobs/jobs_data_basic.py
+++ ArWikiCats/translations/jobs/jobs_data_basic.py
@@ -124,6 +124,7 @@ def _build_military_job_labels(
 # (?<!\w)(shi'a\ muslims|sunni\ muslims|shia\ muslims|shi'a\ muslim|sunni\ muslim|shia\ muslim|episcopalians|evangelical|christians|protestant|anglicans|christian|methodist|religious|venerated|anglican|buddhist|bahá'ís|yazidis|islamic|muslims|muslim|coptic|hindus|jewish|zaydis|saints|hindu|zaydi|sufis|nazi|jews)(?!\w)
 RELIGIOUS_KEYS_PP: GenderedLabelMap = {
     "bahá'ís": {"males": "بهائيون", "females": "بهائيات"},
+    "baháís": {"males": "بهائيون", "females": "بهائيات"},
     "yazidis": {"males": "يزيديون", "females": "يزيديات"},
     "christians": {"males": "مسيحيون", "females": "مسيحيات"},
     "anglican": {"males": "أنجليكيون", "females": "أنجليكيات"},
