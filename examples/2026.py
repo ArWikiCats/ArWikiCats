ba9bc30e2462dedf6ea7b1fa9e0ffb4e897411e6
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-14 09:07:22 +0300

    .

---
+++ examples/2026.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+import sys
+import json
+import time
+from tqdm import tqdm
+from pathlib import Path
+
+
+if _Dir := Path(__file__).parent.parent:
+    sys.path.append(str(_Dir))
+
+from ArWikiCats import print_memory, batch_resolve_labels
+
+file_path = Path(Path(__file__).parent, "2026.json")
+
+time_start = time.time()
+
+with open(file_path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+result = batch_resolve_labels(tqdm(data))
+labels = result.labels
+no_labels = result.no_labels
+
+print(f"total: {len(data)}")
+print(f"labels: {len(labels)}")
+print(f"no_labels: {len(no_labels)}")
+
+time_diff = time.time() - time_start
+print(f"total time: {time_diff} seconds")
+print_memory()
