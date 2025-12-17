49322b8d19eaed4ad61a106d0190acdfa135f39b
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-18 02:11:24 +0300

    Update test_relations_data_json_file.py

--- tests/make2_bots/reslove_relations/test_relations_data_json_file.py
+++ tests/make2_bots/reslove_relations/test_relations_data_json_file.py
@@ -7,18 +7,17 @@ from ArWikiCats.make_bots.reslove_relations.rele import resolve_relations_label
 from ArWikiCats import resolve_label_ar
 
 
-def _load_data(file_name) -> list:
+def _load_data(file_name, chunk_size=3000) -> list:
     file_path = Path(__file__).parent / file_name
     big_data = json.loads(file_path.read_text(encoding="utf-8"))
 
     TEMPORAL_CASES = []
     # split big_data into patches of up to CHUNK_SIZE items each for TEMPORAL_CASES
-    CHUNK_SIZE = 6000
 
     items = list(big_data.items())
-    for idx in range(0, len(items), CHUNK_SIZE):
-        chunk_dict = dict(items[idx : idx + CHUNK_SIZE])
-        name = f"patch_{CHUNK_SIZE}_item_number_{idx // CHUNK_SIZE + 1}"
+    for idx in range(0, len(items), chunk_size):
+        chunk_dict = dict(items[idx : idx + chunk_size])
+        name = f"patch_{chunk_size}_item_number_{idx // chunk_size + 1}"
         TEMPORAL_CASES.append((name, chunk_dict))
     return TEMPORAL_CASES
 
@@ -35,7 +34,7 @@ def test_all_dump(name: str, data: str) -> None:
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
 
 
-TEMPORAL_CASES2 = _load_data("relations_data_empty.json")
+TEMPORAL_CASES2 = _load_data("relations_data_empty.json", 1000)
 
 
 @pytest.mark.parametrize("name,data", TEMPORAL_CASES2)
