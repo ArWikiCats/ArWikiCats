b9041126574d4bcf7fb915abee5a85295a2ea609
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-18 02:15:53 +0300

    Update test to use change_cat and adjust parameters
    Applied the change_cat function to chunk_dict keys in _load_data, increased chunk size for TEMPORAL_CASES2 to 4000, and switched test_empty_dump to use resolve_relations_label instead of resolve_label_ar. These changes improve test coverage and data handling.

--- tests/make2_bots/reslove_relations/test_relations_data_json_file.py
+++ tests/make2_bots/reslove_relations/test_relations_data_json_file.py
@@ -5,6 +5,7 @@ from load_one_data import dump_diff, one_dump_test
 
 from ArWikiCats.make_bots.reslove_relations.rele import resolve_relations_label
 from ArWikiCats import resolve_label_ar
+from ArWikiCats.make_bots import change_cat
 
 
 def _load_data(file_name, chunk_size=3000) -> list:
@@ -17,8 +18,12 @@ def _load_data(file_name, chunk_size=3000) -> list:
     items = list(big_data.items())
     for idx in range(0, len(items), chunk_size):
         chunk_dict = dict(items[idx : idx + chunk_size])
+
+        chunk_dict = {change_cat(x): v for x, v in chunk_dict.items()}
+
         name = f"patch_{chunk_size}_item_number_{idx // chunk_size + 1}"
         TEMPORAL_CASES.append((name, chunk_dict))
+
     return TEMPORAL_CASES
 
 
@@ -34,14 +39,14 @@ def test_all_dump(name: str, data: str) -> None:
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
 
 
-TEMPORAL_CASES2 = _load_data("relations_data_empty.json", 1000)
+TEMPORAL_CASES2 = _load_data("relations_data_empty.json", 4000)
 
 
 @pytest.mark.parametrize("name,data", TEMPORAL_CASES2)
 @pytest.mark.skip2
 def test_empty_dump(name: str, data: str) -> None:
-    expected, diff_result = one_dump_test(data, resolve_label_ar)
-    # expected, diff_result = one_dump_test(data, resolve_relations_label)
+    # expected, diff_result = one_dump_test(data, resolve_label_ar)
+    expected, diff_result = one_dump_test(data, resolve_relations_label)
 
     dump_diff(diff_result, f"test_resolve_relations_label_big_data_{name}")
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
