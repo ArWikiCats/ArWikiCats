6967c4974cd58220028cef98c4f139c4a41e6bf3
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-19 06:35:57 +0300

    Update __init__.py

--- ArWikiCats/new_resolvers/__init__.py
+++ ArWikiCats/new_resolvers/__init__.py
@@ -1,44 +1,9 @@
 
-from .translations_formats.DataModel import (
-    YearFormatData,
-    FormatData,
-    NormalizeResult,
-    MultiDataFormatterBase,
-    MultiDataFormatterBaseV2,
-    FormatDataDouble,
-    FormatDataV2,
-    MultiDataFormatterDataDouble,
-    MultiDataFormatterBaseYear,
-    V3Formats,
-    MultiDataFormatterBaseYearV2,
-    MultiDataFormatterBaseYearV3,
-    MultiDataFormatterYearAndFrom,
-    FormatDataFrom,
-)
 
-from .translations_formats.data_with_time import format_year_country_data, format_year_country_data_v2
-from .translations_formats.data_new_model import format_films_country_data
-from .translations_formats.multi_data import format_multi_data, format_multi_data_v2
+from . import translations_resolvers, translations_resolvers_v2, translations_resolvers_v3i
 
 __all__ = [
-    "V3Formats",
-    "MultiDataFormatterBaseYear",
-    "MultiDataFormatterBaseYearV2",
-    "MultiDataFormatterBaseYearV3",
-    "MultiDataFormatterYearAndFrom",
-    "FormatDataFrom",
-
-    "MultiDataFormatterDataDouble",
-    "FormatDataV2",
-    "FormatDataDouble",
-    "YearFormatData",
-    "FormatData",
-    "NormalizeResult",
-    "MultiDataFormatterBase",
-    "MultiDataFormatterBaseV2",
-    "format_year_country_data",
-    "format_year_country_data_v2",
-    "format_films_country_data",
-    "format_multi_data",
-    "format_multi_data_v2",
+    "translations_resolvers",
+    "translations_resolvers_v2",
+    "translations_resolvers_v3i",
 ]
