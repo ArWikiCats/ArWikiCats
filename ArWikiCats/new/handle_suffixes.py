b1b93cec4980eab0f3f85ae3d1415f4b2abc7a43
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-23 04:54:36 +0300

    Refactor sport category suffix handling logic
    Removed p17_bot_sport_wrap.py and introduced handle_suffixes.py with improved and more flexible functions for resolving sport category suffixes. The new implementation allows for customizable value-label combination and supports additional formatting options.

---
+++ ArWikiCats/new/handle_suffixes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+"""
+from ..helps import logger
+
+
+def normalize_text(text: str) -> str:
+    text = text.lower().replace("category:", "")
+    # text = text.replace("sportspeople", "sports-people")
+    text = text.replace(" the ", " ")
+    # text = text.replace("republic of", "republic-of")
+    if text.startswith("the "):
+        text = text[4:]
+    return text.strip()
+
+
+def combine_value_and_label(
+    value: str,
+    new_label: str,
+    format_key: str="",
+) -> str:
+    """
+    Combine value and new_label based on format_key.
+    Examples:
+    - If format_key is "", return "value new_label".
+    - If format_key is "{}", return value formatted with new_label.
+    - If format_key == "ar", return value formatted with new_label using format_map.
+    """
+    if not format_key:
+        return f"{value} {new_label}"
+
+    if format_key == "{}":
+        return value.format(new_label)
+
+    result = value.format_map({format_key: new_label})
+    return result
+
+
+def resolve_sport_category_suffix_with_mapping(
+    category: str,
+    data: dict[str, str],
+    callback: callable,
+    format_key: str = "",
+) -> str:
+    """."""
+    logger.debug(f"<<yellow>> start resolve_sport_category_suffix_with_mapping: {category=}")
+
+    result = ""
+
+    # category = normalize_text(category)
+    for key, value in data.items():
+        if category.endswith(key):
+            new_category = category[: -len(key)].strip()
+            new_label = callback(new_category)
+            if new_label:
+                result = combine_value_and_label(value, new_label, format_key)
+            break
+
+    if not result:
+        result = callback(category)
+
+    logger.debug(f"<<yellow>> end resolve_sport_category_suffix_with_mapping: {category=}, {result=}")
+    return result
+
+
+__all__ = [
+    "resolve_sport_category_suffix_with_mapping",
+]
