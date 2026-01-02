7b17cf9eb702609f190be5f7ce42fe1ddf40b02b
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-02 06:52:16 +0300

    Update test_event2bot.py

--- tests/main_processers/test_event2bot.py
+++ tests/main_processers/test_event2bot.py
@@ -4,7 +4,7 @@
 
 import pytest
 
-from ArWikiCats.main_processers.event2bot import event2_d2, event2_new2
+from ArWikiCats.main_processers.event2bot import event2_d2
 
 fast_data = {
 }
@@ -13,7 +13,7 @@ fast_data = {
 @pytest.mark.parametrize("category, expected", fast_data.items(), ids=fast_data.keys())
 @pytest.mark.fast
 def test_event2_fast(category: str, expected: str) -> None:
-    label = event2_new2(category)
+    label = event2_d2(category)
     assert label == expected
 
 
