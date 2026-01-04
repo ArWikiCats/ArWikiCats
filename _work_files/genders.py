60d8e8a70c0aba7a82c0859a3f19d104394385ce
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-04 06:38:27 +0300

    Refactor gender jobs data output and add new JSON files
    Updated genders.py to output job data into a new 'genders_data' directory and split two-word job entries into separate JSON files based on word matching. Added jobs_data_multi_one_word.json, jobs_data_multi_two_words.json, and jobs_data_multi_two_words3.json to store the processed job data.

--- _work_files/genders.py
+++ _work_files/genders.py
@@ -52,18 +52,35 @@ for key in keys_in_both_one_word:
         "both_jobs": f"{mens_data[key]} و{womens_data[key]}",
     }
 
-with open(Path(__file__).parent / "jobs_data_multi_one_word.json", "w", encoding="utf-8") as f:
+with open(Path(__file__).parent / "genders_data/jobs_data_multi_one_word.json", "w", encoding="utf-8") as f:
     json.dump(new_data, f, ensure_ascii=False, indent=4)
 
-keys_in_both_2_words = [x for x in keys_in_both if len(mens_data.get(x).split()) == 2]
+keys_in_both_2_words = [x for x in keys_in_both if len(mens_data.get(x).split(" ")) == 2]
 print(f"Keys in both mens and womens data (two words): {len(keys_in_both_2_words)}")
 
-for key in keys_in_both_2_words:
-    new_data[key] = {
-        "job_males": mens_data[key],
-        "job_females": womens_data[key],
-        "both_jobs": f"{mens_data[key]} و{womens_data[key]}",
-    }
+new_data2 = {}
+new_data3 = {}
 
-with open(Path(__file__).parent / "jobs_data_multi_two_words.json", "w", encoding="utf-8") as f:
-    json.dump(new_data, f, ensure_ascii=False, indent=4)
+for key in keys_in_both_2_words:
+    print(key)
+    word1_mens, word2_mens = mens_data[key].split(" ")
+    word1_womens, word2_womens = womens_data[key].split(" ")
+
+    if word2_mens == word2_womens:
+        new_data2[key] = {
+            "job_males": mens_data[key],
+            "job_females": womens_data[key],
+            "both_jobs": f"{word1_mens} و{word1_womens} {word2_mens}",
+        }
+    else:
+        new_data3[key] = {
+            "job_males": mens_data[key],
+            "job_females": womens_data[key],
+            "both_jobs": f"{word1_mens} و{word1_womens} {word2_mens}",
+        }
+
+with open(Path(__file__).parent / "genders_data/jobs_data_multi_two_words.json", "w", encoding="utf-8") as f:
+    json.dump(new_data2, f, ensure_ascii=False, indent=4)
+
+with open(Path(__file__).parent / "genders_data/jobs_data_multi_two_words3.json", "w", encoding="utf-8") as f:
+    json.dump(new_data3, f, ensure_ascii=False, indent=4)
