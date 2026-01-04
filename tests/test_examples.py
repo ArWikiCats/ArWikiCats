dc1e1458b7604d5a3c62769f75e1ae538bc4bac8
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-05 02:10:38 +0300

    Update test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 
 import pytest
-from load_one_data import dump_diff, one_dump_test
+from load_one_data import dump_diff, one_dump_test, dump_same_and_not_same
 
 from ArWikiCats import resolve_arabic_category_label
 
@@ -27,9 +27,8 @@ FILE_PATHS = list(DATA_DIR.glob("*.json"))
 def test_examples_data(example_data: tuple[dict[str, str], str]) -> None:
     data, name = example_data
     expected, diff_result = one_dump_test(data, resolve_arabic_category_label)
-    dump_diff(diff_result, name)
 
-    expected2 = {x: v for x, v in expected.items() if v and x in diff_result}
-    dump_diff(expected2, f"{name}_expected")
+    dump_diff(diff_result, name)
+    dump_same_and_not_same(data, diff_result, name)
 
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
