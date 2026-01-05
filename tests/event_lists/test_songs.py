938c69a27ca15779b758f9d2b58760fb888b2f30
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-05 08:46:17 +0300

    .

---
+++ tests/event_lists/test_songs.py
@@ -0,0 +1,47 @@
+#
+import pytest
+from load_one_data import dump_diff, one_dump_test
+
+from ArWikiCats import resolve_arabic_category_label
+
+data1 = {
+    "Category:Bengali folk songs": "تصنيف:أغاني فولك بنغالي",
+    "Category:Indian folk songs": "تصنيف:أغاني فولك هندي",
+    "Category:American rock music": "تصنيف:موسيقى الروك أمريكي",
+    "Category:Danish country music": "تصنيف:كانتري دنماركي",
+    "Category:Estonian rock music": "تصنيف:موسيقى الروك إستوني",
+    "Category:French electronic songs": "تصنيف:أغاني إليكترونيك فرنسي",
+    "Category:Mongolian traditional music": "تصنيف:موسيقى تقليدية منغولي",
+    "Category:Serbian hip-hop": "تصنيف:هيب هوب صربي",
+    "Christian country music": "تصنيف:كانتري مسيحي",
+    "Christian electronic dance music songs": "تصنيف:أغاني موسيقى الرقص الإلكترونية مسيحي",
+    "Christian electronic dance music": "تصنيف:موسيقى الرقص الإلكترونية مسيحي",
+    "Christian hip-hop songs": "تصنيف:أغاني هيب هوب مسيحي",
+    "Christian hip-hop": "تصنيف:هيب هوب مسيحي",
+    "Christian R&B": "تصنيف:ريذم أند بلوز مسيحي",
+    "Jewish dance": "تصنيف:رقص يهودي",
+    "Jewish folk songs": "تصنيف:أغاني فولك يهودي",
+    "Jewish hip-hop": "تصنيف:هيب هوب يهودي",
+    "Jewish rock songs": "تصنيف:أغاني روك يهودي",
+    "Jewish rock": "تصنيف:روك يهودي"
+}
+
+to_test = [
+    ("test_1", data1),
+]
+
+
+@pytest.mark.parametrize("category, expected", data1.items(), ids=data1.keys())
+@pytest.mark.fast
+def test_1(category: str, expected: str) -> None:
+    label = resolve_arabic_category_label(category)
+    assert label == expected
+
+
+@pytest.mark.parametrize("name,data", to_test)
+@pytest.mark.dump
+def test_peoples(name: str, data: dict[str, str]) -> None:
+    expected, diff_result = one_dump_test(data, resolve_arabic_category_label)
+
+    dump_diff(diff_result, name)
+    assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
