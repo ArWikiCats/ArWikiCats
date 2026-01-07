d3dffad3e9f4c835c70910bddfd05af9262ad546
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-07 03:56:38 +0300

    Update test_prefix_bot_mens.py

--- tests/new_resolvers/jobs_resolvers/prefix_bot/test_prefix_bot_mens.py
+++ tests/new_resolvers/jobs_resolvers/prefix_bot/test_prefix_bot_mens.py
@@ -283,6 +283,6 @@ TEMPORAL_CASES = [
 def test_all_dump(name: str, data: dict[str, str], callback) -> None:
     expected, diff_result = one_dump_test(data, callback)
 
-    # dump_diff(diff_result, name)
-    dump_same_and_not_same(data, diff_result, name)
+    dump_diff(diff_result, name)
+    # dump_same_and_not_same(data, diff_result, name)
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
