779beaf7f27a71b59c9b5d5e21fb0ff4ce28961e
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-07 10:55:03 +0300

    Integrate Work_for_me_main into film and sports bots
    Added Work_for_me_main from old_bots.for_me to films_and_others_bot.py and team_work.py. This enables the use of Work_for_me_main for resolving categories and club labels, improving data handling in both bots.

--- ArWikiCats/make_bots/films_and_others_bot.py
+++ ArWikiCats/make_bots/films_and_others_bot.py
@@ -19,6 +19,7 @@ from .countries_formats import resolved_countries_formats_labels
 from ..old_bots.t4_2018_jobs import te4_2018_Jobs
 from ..old_bots.bot_te_4 import jobs_in_multi_sports
 from ..old_bots.bot_te_4_nat import nat_match
+from ..old_bots.for_me import Work_for_me_main
 from .languages_bot.languages_resolvers import te_language
 # from .lazy_data_bots.bot_2018 import get_pop_All_18
 from .matables_bots.bot import add_to_Films_O_TT, add_to_new_players
@@ -56,6 +57,7 @@ def te_films(category: str) -> str:
         "get_films_key_tyty_new_and_time": lambda k: get_films_key_tyty_new_and_time(k),
         "get_films_key_tyty_new": lambda k: get_films_key_tyty_new(k),
         "jobs_in_multi_sports": lambda k: jobs_in_multi_sports(k),
+        "Work_for_me_main": lambda k: Work_for_me_main(k),
         "Films": lambda k: Films(k),
         # TODO: get_pop_All_18 make some issues, see: tests/test_bug/test_bug_bad_data.py
         # "get_pop_All_18": lambda k: get_pop_All_18(k),
@@ -82,6 +84,7 @@ def te_films(category: str) -> str:
     ]
 
     _add_to_films_o_tt_tables = [
+        "Work_for_me_main",
         "Films",
     ]
 
