1d962a5323e2756d27439f3871aec313cc9499e3
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-14 05:55:14 +0300

    Update test_mans_data.py

--- tests/event_lists/importants/test_mans_data.py
+++ tests/event_lists/importants/test_mans_data.py
@@ -1,17 +1,64 @@
 #
 import pytest
-from load_one_data import dump_diff, dump_diff_text, one_dump_test
+from load_one_data import dump_diff, dump_diff_text, one_dump_test, dump_same_and_not_same
 
 from ArWikiCats import resolve_label_ar
 
-data0 = {}
+from ArWikiCats.new_resolvers.sports_resolvers.nationalities_and_sports import resolve_nats_sport_multi_v2
+
+data0 = {
+    "yemeni abortion law": "قانون الإجهاض اليمني",
+    "yemeni amateur football league": "الدوري اليمني لكرة القدم للهواة",
+    "yemeni army": "الجيش اليمني",
+    "yemeni census": "التعداد اليمني",
+    "yemeni criminal law": "القانون الجنائي اليمني",
+    "yemeni elite cup": "كأس النخبة اليمني",
+    "yemeni family law": "قانون الأسرة اليمني",
+    "yemeni indoor rugby league": "الدوري اليمني للرجبي داخل الصالات",
+    "yemeni indoor wheelchair rugby league": "الدوري اليمني للرجبي على الكراسي المتحركة داخل الصالات",
+    "yemeni invasion": "الغزو اليمني",
+    "yemeni involvement": "التدخل اليمني",
+    "yemeni labour law": "قانون العمل اليمني",
+    "yemeni major indoor rugby league": "الدوري الرئيسي اليمني للرجبي داخل الصالات",
+    "yemeni major indoor wheelchair rugby league": "الدوري الرئيسي اليمني للرجبي على الكراسي المتحركة داخل الصالات",
+    "yemeni military": "الجيش اليمني",
+    "yemeni national party": "الحزب الوطني اليمني",
+    "yemeni national super league": "دوري السوبر اليمني",
+    "yemeni occupation": "الاحتلال اليمني",
+    "yemeni outdoor rugby league": "الدوري اليمني للرجبي في الهواء الطلق",
+    "yemeni outdoor wheelchair rugby league": "الدوري اليمني للرجبي على الكراسي المتحركة في الهواء الطلق",
+    "yemeni pardons": "العفو اليمني",
+    "yemeni population and housing census": "التعداد اليمني للسكان والمساكن",
+    "yemeni population census": "التعداد السكاني اليمني",
+    "yemeni premier division": "الدوري اليمني الممتاز",
+    "yemeni premier league": "الدوري اليمني الممتاز",
+    "yemeni president cup": "كأس الرئيس اليمني",
+    "yemeni presidential pardons": "العفو الرئاسي اليمني",
+    "yemeni professional league": "دوري المحترفين اليمني",
+    "yemeni professional league managers": "مدربو دوري المحترفين اليمني",
+    "yemeni professional rugby league": "دوري الرجبي اليمني للمحترفين",
+    "yemeni professional wheelchair rugby league": "دوري الرجبي على الكراسي المتحركة اليمني للمحترفين",
+    "yemeni referendum": "الاستفتاء اليمني",
+    "yemeni rugby league administrators": "مدراء الدوري اليمني للرجبي",
+    "yemeni rugby league players": "لاعبو الدوري اليمني للرجبي",
+    "yemeni rugby league playerss": "لاعبو الدوري اليمني للرجبي",
+    "yemeni rugby union leagues": "اتحاد دوري الرجبي اليمني",
+    "yemeni solidarity movement": "حركة التضامن اليمني",
+    "yemeni super cup": "كأس السوبر اليمني",
+    "yemeni super league": "دوري السوبر اليمني",
+    "yemeni wheelchair rugby league": "الدوري اليمني للرجبي على الكراسي المتحركة",
+    "yemeni wheelchair rugby league administrators": "مدراء الدوري اليمني للرجبي على الكراسي المتحركة",
+    "yemeni wheelchair rugby league players": "لاعبو الدوري اليمني للرجبي على الكراسي المتحركة",
+    "yemeni wheelchair rugby league playerss": "لاعبو الدوري اليمني للرجبي على الكراسي المتحركة",
+    "yemeni women's rugby league": "الدوري اليمني للرجبي للسيدات",
+    "yemeni women's rugby union": "اتحاد الرجبي اليمني للنساء",
+    "yemeni women's wheelchair rugby league": "الدوري اليمني للرجبي على الكراسي المتحركة للسيدات"
+}
 
 data1 = {
     "australian rugby union": "اتحاد الرجبي الأسترالي",
     "australian women's ice hockey league": "الدوري الأسترالي لهوكي الجليد للسيدات",
-    "brazilian rugby league players": "لاعبو الدوري البرازيلي للرجبي",
     "british rugby union": "اتحاد الرجبي البريطاني",
-    "canadian military": "الجيش الكندي",
     "egyptian league": "الدوري المصري",
     "english football league": "الدوري الإنجليزي لكرة القدم",
     "english rugby league": "الدوري الإنجليزي للرجبي",
@@ -20,9 +67,7 @@ data1 = {
     "hong kong fa cup": "كأس الاتحاد الهونغ الكونغي",
     "indian federation cup": "كأس الاتحاد الهندي",
     "italian rugby union": "اتحاد الرجبي الإيطالي",
-    "japanese army": "الجيش الياباني",
     "lebanese federation cup": "كأس الاتحاد اللبناني",
-    "moroccan army": "الجيش المغربي",
     "north american hockey league": "الدوري الأمريكي الشمالي للهوكي",
     "north american soccer league": "الدوري الأمريكي الشمالي لكرة القدم",
     "oceanian rugby union": "اتحاد الرجبي الأوقيانوسي",
@@ -34,7 +79,6 @@ data1 = {
 data_2 = {
     "argentine grand prix": "جائزة الأرجنتين الكبرى",
     "british open": "المملكة المتحدة المفتوحة",
-    "canadian labour law": "قانون العمل الكندي",
     "croatian ice hockey league": "الدوري الكرواتي لهوكي الجليد",
     "eritrean premier league": "الدوري الإريتري الممتاز",
     "french rugby union leagues": "اتحاد دوري الرجبي الفرنسي",
@@ -49,10 +93,10 @@ data_4 = {
 }
 
 to_test = [
-    ("test_mans_data_0", data0),
-    ("test_mans_data_1", data1),
-    ("test_mans_data_3", data_3),
-    ("test_mans_data_4", data_4),
+    ("test_mans_data_0", data0, resolve_label_ar),
+    ("test_mans_data_1", data1, resolve_nats_sport_multi_v2),
+    ("test_mans_data_3", data_3, resolve_nats_sport_multi_v2),
+    ("test_mans_data_4", data_4, resolve_nats_sport_multi_v2),
 ]
 
 
@@ -63,19 +107,20 @@ def test_mans_data_0(category: str, expected: str) -> None:
 
 @pytest.mark.parametrize("category, expected", data1.items(), ids=data1.keys())
 def test_mans_data_1(category: str, expected: str) -> None:
-    assert resolve_label_ar(category) == expected
+    assert resolve_nats_sport_multi_v2(category) == expected
 
 
 @pytest.mark.parametrize("category, expected", data_3.items(), ids=data_3.keys())
 def test_mans_data_3(category: str, expected: str) -> None:
-    assert resolve_label_ar(category) == expected
+    assert resolve_nats_sport_multi_v2(category) == expected
 
 
-@pytest.mark.parametrize("name,data", to_test)
+@pytest.mark.parametrize("name,data,callback", to_test)
 @pytest.mark.dump
-def test_dump_it(name: str, data: dict[str, str]) -> None:
-    expected, diff_result = one_dump_test(data, resolve_label_ar)
+def test_dump_it(name: str, data: dict[str, str], callback) -> None:
+    expected, diff_result = one_dump_test(data, callback)
     dump_diff(diff_result, name)
 
     # dump_diff_text (expected, diff_result, name)
+    dump_same_and_not_same(data, diff_result, name)
     assert diff_result == expected, f"Differences found: {len(diff_result):,}, len all :{len(data):,}"
