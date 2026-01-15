2b6cf2a3a8bea70e7f5c3fea75ec61b01335cbbc
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-15 05:26:08 +0300

    Add noprint_formats option to print config
    Introduces a new 'noprint_formats' flag to PrintConfig and updates formats_logger to disable logging if either 'noprint' or 'noprint_formats' is set. This allows more granular control over logging output for formats.

--- ArWikiCats/translations_formats/formats_logger.py
+++ ArWikiCats/translations_formats/formats_logger.py
@@ -3,7 +3,7 @@
 from ..config import print_settings
 from ..helps.log import LoggerWrap
 
-logger = LoggerWrap(__name__, disable_log=print_settings.noprint)
+logger = LoggerWrap(__name__, disable_log=print_settings.noprint or print_settings.noprint_formats)
 
 
 __all__ = [
