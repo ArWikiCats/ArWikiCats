4426317bd0c694c6555b3dfe3eb7942705d3648d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-16 06:27:58 +0300

    Add logging to resolved_names_with_sports function
    Introduced debug and info logging in the resolved_names_with_sports function to improve traceability of category resolution. Also removed commented import and usage of resolved_names_with_sports in films_and_others_bot.py for code clarity.

--- ArWikiCats/new_resolvers/countries_names_with_sports/__init__.py
+++ ArWikiCats/new_resolvers/countries_names_with_sports/__init__.py
@@ -1,4 +1,5 @@
 
+from ...helps import logger
 from . import (
     p17_bot_sport,
     p17_sport_to_move_under,
@@ -6,6 +7,7 @@ from . import (
 
 
 def resolved_names_with_sports(normalized_category) -> str:
+    logger.debug(f"<><><><><><> <<green>> Trying resolved_names_with_sports for: {normalized_category=}")
     resolved_label = (
         #  [yemen international soccer players] : "تصنيف:لاعبو منتخب اليمن لكرة القدم",
         # countries_names.resolve_by_countries_names(normalized_category) or
@@ -16,5 +18,8 @@ def resolved_names_with_sports(normalized_category) -> str:
         # [yemen international soccer players] : "تصنيف:لاعبو كرة قدم دوليون من اليمن",
         or p17_bot_sport.get_p17_with_sport_new(normalized_category)
         or ""
+    )
+    logger.info_if_or_debug(
+        f"<<yellow>> end countries_names_resolvers: {normalized_category=}, {resolved_label=}", resolved_label
     )
     return resolved_label
