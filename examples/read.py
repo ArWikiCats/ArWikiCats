264caf70dc782575c4fc604cf4dd5ab8b534a18e
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-17 06:09:09 +0300

    Update read.py

--- examples/read.py
+++ examples/read.py
@@ -15,13 +15,15 @@ json_files = list((dir / "big_data").glob("*.json"))
 for file in tqdm(json_files, desc="Processing JSON files"):
     with open(file, "r", encoding="utf-8") as f:
         json_data = json.load(f)
-
+    json_data_original = json_data.copy()
     for key, value in json_data.copy().items():
         if "endings" in key:
             f_data[key] = value
             del json_data[key]
-    with open(file, "w", encoding="utf-8") as f:
-        json.dump(json_data, f, ensure_ascii=False, indent=4)
+    if json_data_original != json_data:
+        with open(file, "w", encoding="utf-8") as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
 
-with open(f_path, "w", encoding="utf-8") as f:
-    json.dump(f_data, f, ensure_ascii=False, indent=4)
+if f_data:
+    with open(f_path, "w", encoding="utf-8") as f:
+        json.dump(f_data, f, ensure_ascii=False, indent=4)
