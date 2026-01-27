ec069ff3d02e593aa73ad618b558df505e61572f
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-27 07:48:30 +0300

    Update test_import_no_cycle.py

--- tests/unit/translations_formats/test_import_no_cycle.py
+++ tests/unit/translations_formats/test_import_no_cycle.py
@@ -14,7 +14,7 @@ data_fast = {
 
 
 @pytest.mark.parametrize("category, not_expected", data_fast.items(), ids=data_fast.keys())
-@pytest.mark.fast
+@pytest.mark.skip2
 # DON'T COPY THIS
 def test_get_country2_one(category: str, not_expected: str) -> None:
     label = resolve_label_ar(category)
