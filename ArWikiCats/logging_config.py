097bea94c156964cf63707e4fc0de20d9245b3fb
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-30 06:40:58 +0300

    Enhance console log format with more context
    Updated the console log formatter to include filename, line number, and function name for improved debugging context.

--- ArWikiCats/logging_config.py
+++ ArWikiCats/logging_config.py
@@ -143,7 +143,8 @@ def setup_logging(level: str = "INFO", log_file: Optional[str] = None) -> None:
     # Create color formatter for console
     console_formatter = colorlog.ColoredFormatter(
         # fmt="%(log_color)s%(asctime)s - %(name)s - %(levelname)-8s%(reset)s %(message)s",
-        fmt="%(name)s - %(lineno)s - %(log_color)s%(levelname)-8s%(reset)s %(message)s",
+        # fmt="%(name)s:%(lineno)s - %(log_color)s%(levelname)-s %(reset)s%(message)s",
+        fmt="%(filename)s:%(lineno)s %(funcName)s() - %(log_color)s%(levelname)-s %(reset)s%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         log_colors={
             "DEBUG": "cyan",
@@ -155,7 +156,9 @@ def setup_logging(level: str = "INFO", log_file: Optional[str] = None) -> None:
         secondary_log_colors={},
         style="%",
     )
+    # message colorizer
     console_formatter.formatMessage = wrap_color_messages(console_formatter.formatMessage)
+
     # Console handler with colors
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(console_formatter)
