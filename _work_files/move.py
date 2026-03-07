4087bd0743381a2ad2eb38f81ea92e76d7c2878b
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-07 07:12:13 +0300

    Update move.py

--- _work_files/move.py
+++ _work_files/move.py
@@ -7,6 +7,16 @@ https://quarry.wmcloud.org/query/99983
 import json
 from pathlib import Path
 
+json_paths = Path("D:/categories_bot/make2_new/_work_files/genders_data").glob("*.json")
+json_data = {}
+for file in json_paths:
+    data = json.loads(file.read_text(encoding="utf-8"))
+    json_data.update({
+        v["job_males"] : v["both_jobs"]
+        for v in data.values()
+    })
+
+
 SAVE_PATH = Path("D:/categories_bot/make2_new/_work_files/move.wiki")
 CATS_PATH = Path("D:/categories_bot/make2_new/examples/data/males.json")
 CATS_DATA = json.loads(CATS_PATH.read_text(encoding="utf-8"))
@@ -14,15 +24,21 @@ CATS_DATA = json.loads(CATS_PATH.read_text(encoding="utf-8"))
 rows = []
 for n, cat in enumerate(CATS_DATA.values(), 1):
     cat_new = cat.replace(" ذكور ", " ")
-    line = f"| {n} | [[:{cat}]] | [[:{cat_new}]]"
+    cat2 = cat.replace("تصنيف:", "").split("ذكور")[0].strip()
+    cat_both = json_data.get(cat2)
+    if cat_both:
+        cat_both = cat_new.replace(cat2, cat_both)
+        cat_both = f"[[:{cat_both}]]"
+    line = f"| {n} | [[:{cat}]] | [[:{cat_new}]] | {cat_both} "
     rows.append(line)
 
 wikitext = """
 {|
 |-
 !#
-| من
-| إلى
+| 1
+| 2
+| 3
 |-
 """
 
