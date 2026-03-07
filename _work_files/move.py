b2f873c7958723c07b9030c307486d11b5ac0e54
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-07 07:23:30 +0300

    .

--- _work_files/move.py
+++ _work_files/move.py
@@ -17,35 +17,33 @@ for file in json_paths:
     })
 
 
-SAVE_PATH = Path("D:/categories_bot/make2_new/_work_files/move.wiki")
 CATS_PATH = Path("D:/categories_bot/make2_new/examples/data/males.json")
 CATS_DATA = json.loads(CATS_PATH.read_text(encoding="utf-8"))
 
+CATS_DATA_SORTED_BY_VALUE = dict(sorted(CATS_DATA.items(), key=lambda x: x[1]))
+
 rows = []
-for n, cat in enumerate(CATS_DATA.values(), 1):
+for n, cat in enumerate(CATS_DATA_SORTED_BY_VALUE.values(), 1):
     cat_new = cat.replace(" ذكور ", " ")
     cat2 = cat.replace("تصنيف:", "").split("ذكور")[0].strip()
-    cat_both = json_data.get(cat2)
-    if cat_both:
-        cat_both = cat_new.replace(cat2, cat_both)
-        cat_both = f"[[:{cat_both}]]"
-    line = f"| {n} | [[:{cat}]] | [[:{cat_new}]] | {cat_both} "
+    cat_both = json_data.get(cat2, "")
+    if not cat_both:
+        continue
+    cat_both = cat_new.replace(cat2, cat_both)
+    cat_both = f"[[:{cat_both}]]"
+    line = f"! {n}\n| [[:{cat}]]\n| [[:{cat_new}]]\n| {cat_both} "
     rows.append(line)
 
-wikitext = """
-{| class="wikitable sortable"
-|-
-!#
-| 1
-| 2
-| 3
-|-
-"""
+# for each 1000 item save into file
+LIMIT = 1500
+for i in range(0, len(rows), LIMIT):
+    new_rows = rows[i:i+LIMIT]
+    wikitext = """{| class="wikitable sortable" \n|- \n!# \n! 1 \n! 2 \n! 3 \n|-\n"""
 
-wikitext += "\n|-\n".join(rows)
+    wikitext += "\n|-\n".join(new_rows)
 
-wikitext += """
-|}
-"""
-with open(SAVE_PATH, "w", encoding="utf-8") as f:
-    f.write(wikitext)
+    wikitext += """\n|}"""
+    file_number = i // LIMIT + 1
+    SAVE_PATH = Path(f"D:/categories_bot/make2_new/_work_files/move_{file_number}.wiki")
+    with open(SAVE_PATH, "w", encoding="utf-8") as f:
+        f.write(wikitext)
