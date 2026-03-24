47631f43aa7f0b4e1c71b9f3768eb6374b6b4498
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-24 04:02:06 +0300

    Update len_print.py

--- ArWikiCats/helps/len_print.py
+++ ArWikiCats/helps/len_print.py
@@ -60,21 +60,46 @@ def save_data(bot: str, tab: Mapping) -> None:
 
 def data_len(
     bot: str,
-    tab: Mapping[str, int | float],
+    tab: Mapping[str, Any],
 ) -> None:
-    """Collect and optionally save size statistics for the given bot data."""
+    """Collect and optionally save size statistics for the given bot data.
+
+    Supports both raw objects (dicts, lists) and pre-calculated lengths (integers).
+    - For raw objects: calculates len() and sys.getsizeof() normally
+    - For pre-calculated lengths (int/float): uses the value as count, size marked as 'N/A'
+    """
 
     save_data_path = get_save_path()
     if not save_data_path:
         return
 
-    data = {
-        x: {
-            "count": len(v) if not isinstance(v, int) else v,
-            "size": format_size(x, sys.getsizeof(v), {}),
-        }
-        for x, v in tab.items()
-    }
+    data = {}
+    for x, v in tab.items():
+        if isinstance(v, (int, float)):
+            # Pre-calculated length - use value directly as count
+            data[x] = {
+                "count": v,
+                "size": "N/A",
+            }
+        elif isinstance(v, (dict, list)):
+            # Raw object - calculate len() and getsizeof() normally
+            data[x] = {
+                "count": len(v),
+                "size": format_size(x, sys.getsizeof(v), {}),
+            }
+        else:
+            # Other types - try to get len() if possible
+            try:
+                data[x] = {
+                    "count": len(v),
+                    "size": format_size(x, sys.getsizeof(v), {}),
+                }
+            except (TypeError, AttributeError):
+                data[x] = {
+                    "count": 1,
+                    "size": format_size(x, sys.getsizeof(v), {}),
+                }
+
     save_data(bot, tab)
 
     if not data:
